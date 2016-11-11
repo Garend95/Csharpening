2a4c7b9b22320d0d8ebdd2d314ba170db82e8062
Garend95 <garend95@yahoo.com>
2016-11-11 18:22:40 +0400

    falsify method for turning array elements to false by default. printStatus method which prints status of elements

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -17,7 +17,9 @@ namespace test
 
 			bool[][] t_Square = new bool[w][];
 			for(int i = 0; i < w; i++) t_Square[i] = new bool[h];
- 
+
+            theatre.falsify(t_Square ,height: h, width: w);
+            theatre.printStatus(t_Square, height: h, width: w);
 		}
 
 		public void parameters (out int h, out int w, out int a)
@@ -31,5 +33,28 @@ namespace test
 			Console.WriteLine ("Lastly, insert the length of the side of the square covering it.");
 			a = Convert.ToInt32 (Console.ReadLine ());
 		}
+
+        public void falsify (bool[][] t_square, int height , int width)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++) t_square[i][j] = false;
+            }
+        }
+
+        public void printStatus (bool[][] t_square, int height, int width)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(" {0} ", t_square[i][j]);
+                }
+
+                Console.WriteLine("");
+            }
+
+            
+        }
 	}
 }
