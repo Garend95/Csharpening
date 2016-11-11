aad290d7d42164b9bc9a8aaf0e563698c08a44cb
garen <garend95@yahoo.com>
2016-11-11 04:22:08 +0400

    * test.userprefs: Method with out parameters created for taking   theatre measurements
    * Program.cs:
    * test.exe:
    * test.exe.mdb:
    * test.exe:
    * test.exe.mdb:
    * test.csproj.FilesWrittenAbsolute.txt:

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
-
+//First exercise from code forces
 namespace test
 {
 	class MainClass
 	{
+
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
-			Console.WriteLine ("Testing for commit");
+			int h, w, a;
+
+			MainClass theatre = new MainClass ();
+
+			theatre.parameters (out h, out w, out a);
+
+			Console.WriteLine ("the height {0}, the width {1}, the side {2}", h, w, a);
+
+		}
+
+		public void parameters (out int h, out int w, out int a)
+		{
+			Console.WriteLine ("Please insert the height of the theatre square");
+			h = Convert.ToInt32 (Console.ReadLine ());
+
+			Console.WriteLine ("Now the width");
+			w = Convert.ToInt32 (Console.ReadLine ());
+
+			Console.WriteLine ("Lastly, insert the length of the side of the square covering it.");
+			a = Convert.ToInt32 (Console.ReadLine ());
 		}
 	}
 }
