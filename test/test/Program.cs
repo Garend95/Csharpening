530438e130a5bbcf130bbd4dd7527b26d7954000
Garen Danageozian <Garen@localhost.localdomain>
2016-12-01 23:41:55 +0400

    added symbol returning and printing  methods, tweaked fill and findAndfill's parameters to adapt, changed findAndFill to int and added ability to return number of required tiles

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -19,8 +19,14 @@ namespace test
 			for(int i = 0; i < h; i++) t_Square[i] = new unit[w];
 
             theatre.uncover(t_Square ,height: h, width: w);
-            theatre.findAndFill(t_Square, height: h, width: w, side: a);
+            int result = theatre.findAndFill(t_Square, height: h, width: w, side: a);
+            
             //theatre.printStatus(t_Square, height: h, width: w);
+            Console.WriteLine("We can represent the theatre square covered by tiles of side length {0} as", a);
+
+            theatre.printSymbols(t_Square, height: h, width: w);
+
+            Console.WriteLine("Showing that we can cover the square using {0} tiles.", result);
 		}
 
 		public void parameters (out int h, out int w, out int a)
@@ -49,7 +55,7 @@ namespace test
             {
                 for (int j = 0; j < width; j++)
                 {
-                    Console.Write(" {0} ", t_square[i][j].covered);
+                    Console.Write("  {0}  ", t_square[i][j].covered);
                 }
 
                 Console.WriteLine("");
@@ -58,24 +64,28 @@ namespace test
             
         }
 
-        public void findAndFill (unit[][] t_square, int height, int width, int side)
+        public int findAndFill (unit[][] t_square, int height, int width, int side)
         {
-            
+            int symbolIndex = 0;
+            int tileCount = 0;
             for (int m = 0; m < height; m++)
             {
             	for (int n = 0; n < width; n++)
                 {
                     if(t_square[m][n].covered == false)
                     {
-                        fill(t_square, height, width, side, m, n);
+                        fill(t_square, height, width, side, m, n, symbolIndex);
+                        tileCount++;
+                        symbolIndex++;
+                        if(symbolIndex == 7) symbolIndex = 0;
                     }
                 }
             }
             
-            
+            return tileCount;
         }
 
-        public void fill (unit[][] t_square, int height, int width, int side, int startingHeight, int startingWidth)
+        public void fill (unit[][] t_square, int height, int width, int side, int startingHeight, int startingWidth, int index)
         {
              
             for (int i = startingHeight; i < height && i < startingHeight + side; i++)
@@ -84,17 +94,63 @@ namespace test
                 for (int j = startingWidth; j < width && j < startingWidth + side; j++)
                 {
                     t_square[i][j].covered = true;
+
+                    t_square[i][j].symbol = makeSymbol(index);
                     //Console.WriteLine("width ran");
                 }
 
                 
             }
             
-            printStatus(t_square, height, width);
+            //printStatus(t_square, height, width);
                
         }
 
-        //public char makeSymbol (unit[][] t_square, int i, int j)
+        public char makeSymbol (int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 'A';
+                    
+                case 1:
+                    return 'B';
+                    
+                case 2:
+                    return 'C';
+                    
+                case 3:
+                    return 'D';
+                    
+                case 4:
+                    return 'E';
+                    
+                case 5:
+                    return 'F';
+                    
+                case 6:
+                    return 'G';
+                    
+                default:
+                    return 'm';
+                    
+            }
+        }
+
+        public void printSymbols (unit[][] t_square, int height, int width)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("{0} ", t_square[i][j].symbol);
+                }
+
+                Console.WriteLine("");
+            }
+
+            
+        }
 	}
 
     struct unit
