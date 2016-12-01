d3dbad7152185a4d5d72b553b1d9690a793be61d
Garen Danageozian <Garen@localhost.localdomain>
2016-12-01 22:55:05 +0400

    findAndFill method defined for locating uncovered parts, fill method to fill from a starting point until side length, small correction in array definition

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -15,11 +15,12 @@ namespace test
 
 			//Console.WriteLine ("the height {0}, the width {1}, the side {2}", h, w, a);
 
-			unit[][] t_Square = new unit[w][];
-			for(int i = 0; i < w; i++) t_Square[i] = new unit[h];
+			unit[][] t_Square = new unit[h][];
+			for(int i = 0; i < h; i++) t_Square[i] = new unit[w];
 
-            theatre.falsify(t_Square ,height: h, width: w);
-            theatre.printStatus(t_Square, height: h, width: w);
+            theatre.uncover(t_Square ,height: h, width: w);
+            theatre.findAndFill(t_Square, height: h, width: w, side: a);
+            //theatre.printStatus(t_Square, height: h, width: w);
 		}
 
 		public void parameters (out int h, out int w, out int a)
@@ -34,21 +35,21 @@ namespace test
 			a = Convert.ToInt32 (Console.ReadLine ());
 		}
 
-        public void falsify (unit[][] t_square, int height , int width)
+        public void uncover (unit[][] t_square, int height , int width)
         {
-            for (int i = 0; i < width; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < height; j++) t_square[i][j].covered = false;
+                for (int j = 0; j < width; j++) t_square[i][j].covered = false;
             }
         }
 
         public void printStatus (unit[][] t_square, int height, int width)
         {
-            for (int i = 0; i < width; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    Console.Write(" {0} ", t_square[i][j]);
+                    Console.Write(" {0} ", t_square[i][j].covered);
                 }
 
                 Console.WriteLine("");
@@ -56,6 +57,44 @@ namespace test
 
             
         }
+
+        public void findAndFill (unit[][] t_square, int height, int width, int side)
+        {
+            
+            for (int m = 0; m < height; m++)
+            {
+            	for (int n = 0; n < width; n++)
+                {
+                    if(t_square[m][n].covered == false)
+                    {
+                        fill(t_square, height, width, side, m, n);
+                    }
+                }
+            }
+            
+            
+        }
+
+        public void fill (unit[][] t_square, int height, int width, int side, int startingHeight, int startingWidth)
+        {
+             
+            for (int i = startingHeight; i < height && i < startingHeight + side; i++)
+            {
+                //Console.WriteLine("height ran");
+                for (int j = startingWidth; j < width && j < startingWidth + side; j++)
+                {
+                    t_square[i][j].covered = true;
+                    //Console.WriteLine("width ran");
+                }
+
+                
+            }
+            
+            printStatus(t_square, height, width);
+               
+        }
+
+        //public char makeSymbol (unit[][] t_square, int i, int j)
 	}
 
     struct unit
