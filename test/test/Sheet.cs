2edbfbe0340fda4fe8934daeec6185818946e1fb
Garend95 <garend95@gmail.com>
2017-05-19 14:40:41 +0400

    Input formats added, all input is made uppercase.

--- test/test/Sheet.cs
+++ test/test/Sheet.cs
@@ -10,8 +10,16 @@ namespace test
     {
         public static void Main(string[] args)
         {
+           
+            bool format1; // ex: row48col24 or col79row23532
+            bool format2; // ex: aaa67 or idg253
+
             Console.WriteLine("Please enter the spreadsheet coordinates in either format");  ///Row112col333 or AA14
-            string answer = Console.ReadLine();
+            
+            
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            Console.WriteLine(input);
         }
     }
 }
